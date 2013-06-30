28be573d7b04e56c84979031296950fad968e5ce
Nandor Sivok <nandor@gawker.com>
2013-06-30 18:56:49 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='AnsibleShell',
+    name='ansible-shell',
     version='0.0.1',
     author='Nandor Sivok',
     author_email='dominis@haxor.hu',
     scripts=['ansible-shell.py'],
     url='https://github.com/dominis/ansible-shell',
     description='Interactive shell for ansible',
-    long_description=open('README.md').read(),
     install_requires=[
         "ansible >= 1.2"
     ],
