1e8ec23b3b558631bc8f912bd4f278394e0d04a0
Nandor Sivok <nandor@gawker.com>
2013-06-30 19:32:52 +0200

    setup-py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='Nandor Sivok',
     author_email='dominis@haxor.hu',
-    scripts=['ansible-shell.py'],
+    scripts=['ansible-shell'],
     url='https://github.com/dominis/ansible-shell',
     description='Interactive shell for ansible',
     install_requires=[
