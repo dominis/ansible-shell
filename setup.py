7d1f5304e3008de52c53c3ab917e53661d156754
Nandor Sivok <dominis@haxor.hu>
2015-08-06 00:08:28 +0200

    update req

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     url='https://github.com/dominis/ansible-shell',
     description='Interactive shell for ansible',
     install_requires=[
-        "ansible >= 1.3"
+        "ansible >= 1.9"
     ],
 )
